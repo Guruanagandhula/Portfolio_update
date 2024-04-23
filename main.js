ccefdb82b0736dd6b1e5c37caafe403bbd446926
Guru <156224291+Guruanagandhula@users.noreply.github.com>
2024-04-23 03:57:07 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+import "./style.css";
+import Experience from "./Experience/Experience.js";
+
+const experience = new Experience(document.querySelector(".experience-canvas"));
+
+document.addEventListener("DOMContentLoaded", function() {
+    var audioButton = document.getElementById("audioButton");
+    var audio = document.getElementById("audio");
+
+    audioButton.addEventListener("click", function() {
+        if (audio.paused) {
+            audio.play();
+            audioButton.src = "images/volume.png";
+        } else {
+            audio.pause();
+            audioButton.src = "images/mute.png";
+        }
+    });
+    audio.addEventListener("ended", function() {
+        audio.currentTime = 0; 
+        audio.play(); 
+    });
+
+});
